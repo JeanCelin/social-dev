dff718341252336a114ad181f78cc7c8910863c1
Jean Celin <jean_estudos@hotmail.com>
2023-11-09 13:47:22 -0300

    criado componente de imagem

---
+++ pages/login.jsx
@@ -0,0 +1,10 @@
+import ImageWithSpace from '../src/components/layout/ImageWithSpace'
+
+function LoginPage () {
+  return (
+    <ImageWithSpace>
+      Teste
+    </ImageWithSpace>
+  )
+}
+export default LoginPage
