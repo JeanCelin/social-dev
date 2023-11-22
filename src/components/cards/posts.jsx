3817c911bb875087b23eab7554f4b2e74c92a946
Jean Celin <jean_estudos@hotmail.com>
2023-11-22 11:30:25 -0300

    criado edição de posts

--- src/components/cards/posts.jsx
+++ src/components/cards/posts.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import styled from 'styled-components'
 import moment from 'moment'
 import  axios from 'axios'
@@ -6,6 +7,7 @@ import { useSWRConfig} from 'swr'
 
 
 import Menu from '../navigation/Menu'
+import EditPost from './editPost'
 
 
 const PostContainer = styled.div`
@@ -33,9 +35,16 @@ const ContainerMenu = styled.div`
 
 function Post ({ text, user, date, isOwner, id}) { 
   const { mutate } = useSWRConfig()
+  const [editPost, setEditPost] = useState(false)
 
   const handleEdit = async  () => {
-    console.log("EDITAR PUBLICAÇÃO ")
+    setEditPost(true)
+  }
+
+  const handleSaveEdit = () => {
+    setEditPost(false)
+    mutate(`${process.env.NEXT_PUBLIC_API_URL}/api/post`)
+
   }
 
   const handleDelete = async () => {
@@ -78,7 +87,8 @@ function Post ({ text, user, date, isOwner, id}) {
       <StyledUsername>@{user}</StyledUsername>
       <StyledDate>{moment(date).format('LLL')}</StyledDate>
       <ContainerText>
-        {text}
+        {!editPost && text}
+        {editPost && <EditPost id={id} text={text} onSave ={handleSaveEdit}/>}
       </ContainerText>
     </PostContainer>
    
